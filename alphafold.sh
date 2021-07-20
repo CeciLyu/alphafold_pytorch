b4eb39e7c3d6d01311eb7aca9be32d3e6fa6172f
CeciLyu <87684555+CeciLyu@users.noreply.github.com>
2021-07-19 22:48:20 -0500

    Changed DIR in alphafold.sh

--- alphafold.sh
+++ alphafold.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 TARGET="T1019s2"
-TARGET_FILE="test_data/${TARGET}.pkl"
-MODEL_DIR="model"
-OUTPUT_DIR="${TARGET}_out"
+TARGET_FILE="../../casp13_data/${TARGET}.pkl"
+MODEL_DIR="../../model"
+OUTPUT_DIR="../../Results/${TARGET}_out"
 
 echo -e "Saving output to ${OUTPUT_DIR}/\n"
 
